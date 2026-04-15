40a8428b70337a85cf39800902fb402a54226ad8
René Schwaiger <sanssecours@me.com>
2026-04-15 16:59:18 +0200

    Converter: Parse name

--- wpkonverter/scripts/converter.py
+++ wpkonverter/scripts/converter.py
@@ -4,8 +4,15 @@
 
 from argparse import ArgumentParser, Namespace
 from csv import DictReader
+from re import split
+from typing import NamedTuple
 
 from wpkonverter.cli import file_exists
+
+# -- Types --------------------------------------------------------------------
+
+type Remainder = str
+type Parsed[T] = tuple[T, Remainder]
 
 # -- Functions ----------------------------------------------------------------
 
@@ -31,6 +38,35 @@ def get_arguments() -> Namespace:
     return parser.parse_args()
 
 
+def parse_name(text: str) -> Parsed[str]:
+    """Parse participation name
+
+    Args:
+
+        text:
+
+            The input that should be parsed
+
+    Returns:
+
+        A tuple containing the name and the remainder of the input text
+
+    """
+
+    _, remainder = split(r"Teilnehmerin/Teilnehmer:\s*", text, maxsplit=1)
+    name, remainder = split(r"\r?\n", remainder, maxsplit=1)
+    return name, remainder
+
+
+# -- Classes ------------------------------------------------------------------
+
+
+class RegistrationData(NamedTuple):
+    """Store parsed registration data"""
+
+    name: str
+
+
 # -- Main ---------------------------------------------------------------------
 
 
@@ -41,6 +77,14 @@ def main():
 
     with open(filepath, newline="", encoding="utf8") as csvfile:
         reader = DictReader(csvfile)
+        parsed_data = []
         for row in reader:
-            print(row["Text"])
+            text = row["Text"]
+            print(text)
             print("—" * 50)
+            name, _ = parse_name(text)
+            parsed_data.append(RegistrationData(name=name))
+
+    print("Parsed Data:\n")
+    for record in parsed_data:
+        print(record)
