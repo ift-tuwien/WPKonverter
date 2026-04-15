1b5c8fdbce2246b6853f71a344a1decde39a9946
René Schwaiger <sanssecours@me.com>
2026-04-15 15:19:32 +0200

    Converter: Add filepath argument

--- wpkonverter/scripts/converter.py
+++ wpkonverter/scripts/converter.py
@@ -1,12 +1,43 @@
 # -- Import -------------------------------------------------------------------
 
+from argparse import ArgumentParser, Namespace
 from csv import DictReader
+
+from wpkonverter.cli import file_exists
 
 # -- Functions ----------------------------------------------------------------
 
 
+def get_arguments() -> Namespace:
+    """Parse command line arguments
+
+    Returns:
+
+        An object that contains the given command line arguments
+
+    """
+
+    parser = ArgumentParser(
+        description="Extract data from WPK registration mails"
+    )
+    parser.add_argument(
+        "filepath",
+        type=file_exists,
+        help="WPK mail information in CSV format",
+    )
+
+    return parser.parse_args()
+
+
+# -- Main ---------------------------------------------------------------------
+
+
 def main():
-    with open("wpk.csv", newline="", encoding="utf8") as csvfile:
+    """Convert WPK mail registration data"""
+
+    filepath = get_arguments().filepath
+
+    with open(filepath, newline="", encoding="utf8") as csvfile:
         reader = DictReader(csvfile)
         for row in reader:
             print(row["Text"])
