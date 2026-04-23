2b57ddd30a4ddbb68718604be677a5b988b88928
René Schwaiger <sanssecours@me.com>
2026-04-23 13:15:40 +0200

    Parsing: Add grammar for remainder of mail

--- wpkonverter/parsing.py
+++ wpkonverter/parsing.py
@@ -8,6 +8,7 @@ from pyparsing import (
     Literal,
     OneOrMore,
     Regex,
+    StringEnd,
     Suppress,
 )
 
@@ -21,21 +22,86 @@ from_start = Suppress(Literal("Von:"))
 subject_start = Suppress(Literal("Betreff:"))
 participant_start = Suppress(Literal("Teilnehmerin/Teilnehmer:"))
 organization_start = Suppress(Literal("Unternehmen/ Bildungsinstitut:"))
+contact_start = Suppress(Literal("Kontakt:"))
+sponsor_start = Suppress(
+    Literal("Sind Sie daran interessiert, Sponsor oder Redner zu werden?:")
+)
+message_start = Suppress(Literal("Nachricht:"))
+end = Suppress(Literal("""--
+This is a notification that a contact form was submitted on your website (Wiener Produktionstechnik-Kongress https://wpk.conf.tuwien.ac.at).
+"""))
+
+
+# ========
+# = From =
+# ========
 
 text_from = Combine(OneOrMore(~subject_start + char)).set_parse_action(rstrip)
 from_ = from_start + text_from
+
+# ===========
+# = Subject =
+# ===========
 
 text_subject = Combine(OneOrMore(~participant_start + char)).set_parse_action(
     rstrip
 )
 subject = subject_start + text_subject
 
+# ===============
+# = Participant =
+# ===============
+
 text_participant = Combine(
     OneOrMore(~organization_start + char)
 ).set_parse_action(rstrip)
 participant = participant_start + text_participant
 
-mail = from_ + subject + participant
+# ================
+# = Organization =
+# ================
+
+text_organization = Combine(OneOrMore(~contact_start + char)).set_parse_action(
+    rstrip
+)
+organization = organization_start + text_organization
+
+# ===========
+# = Contact =
+# ===========
+
+text_contact = Combine(OneOrMore(~sponsor_start + char)).set_parse_action(
+    rstrip
+)
+contact = contact_start + text_contact
+
+# ===========
+# = Sponsor =
+# ===========
+
+text_sponsor = Combine(OneOrMore(~message_start + char)).set_parse_action(
+    rstrip
+)
+sponsor = sponsor_start + text_sponsor
+
+# ===========
+# = Message =
+# ===========
+
+text_message = Combine(OneOrMore(~end + char)).set_parse_action(rstrip)
+message = message_start + text_message
+
+mail = (
+    from_
+    + subject
+    + participant
+    + organization
+    + contact
+    + sponsor
+    + message
+    + end
+    + StringEnd()
+)
 
 # -- Functions ----------------------------------------------------------------
 
