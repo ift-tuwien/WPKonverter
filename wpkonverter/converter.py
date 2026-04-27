0f6afa1b7ca2c5033a56e071cb572fb1924437c6
René Schwaiger <sanssecours@me.com>
2026-04-27 14:24:58 +0200

    Parsing: Move error message function

--- wpkonverter/converter.py
+++ wpkonverter/converter.py
@@ -10,10 +10,10 @@ from openpyxl import Workbook
 from openpyxl.styles import Font
 from openpyxl.utils import get_column_letter
 from openpyxl.worksheet.dimensions import ColumnDimension, DimensionHolder
-from pyparsing import col, lineno, ParseException, ParseResults
+from pyparsing import ParseException, ParseResults
 
 from wpkonverter.cli import file_exists
-from wpkonverter.parsing import mail, mail_attributes
+from wpkonverter.parsing import generate_error_message, mail, mail_attributes
 
 # -- Functions ----------------------------------------------------------------
 
@@ -46,42 +46,6 @@ def get_arguments() -> Namespace:
     )
 
     return parser.parse_args()
-
-
-def generate_error_message(text: str, error: ParseException) -> str:
-    """Generate a human readable parsing error message
-
-    Args:
-
-        text:
-
-            The parsed text that produced an error
-
-        error:
-
-            The error that occurred while parsing ``text``
-
-    Returns:
-
-        An error message that shows the user where the parsing error occurred
-
-    """
-
-    line_number = lineno(error.loc, text)
-    column_number = col(error.loc, text)
-    lines = text.splitlines()
-    quote_symbol = "> "
-    error_indent = " " * (len(quote_symbol) + column_number)
-
-    error_message: list[str] = []
-    for line in lines[line_number - 5 : line_number]:
-        error_message.append(f"{quote_symbol}{line}")
-    error_message.append(f"{error_indent}^")
-    error_message.append(f"{error_indent}{error}")
-    for line in lines[line_number : line_number + 1]:
-        error_message.append(f"{quote_symbol}{line}")
-
-    return "\n".join(error_message)
 
 
 def store_data_workbook(parsed_mails: list[ParseResults]) -> None:
--- wpkonverter/parsing.py
+++ wpkonverter/parsing.py
@@ -3,12 +3,54 @@
 # -- Imports ------------------------------------------------------------------
 
 from pyparsing import (
+    col,
     Combine,
+    lineno,
     Literal,
     OneOrMore,
+    ParseException,
     Regex,
     Suppress,
 )
+
+# -- Functions ----------------------------------------------------------------
+
+
+def generate_error_message(text: str, error: ParseException) -> str:
+    """Generate a human readable parsing error message
+
+    Args:
+
+        text:
+
+            The parsed text that produced an error
+
+        error:
+
+            The error that occurred while parsing ``text``
+
+    Returns:
+
+        An error message that shows the user where the parsing error occurred
+
+    """
+
+    line_number = lineno(error.loc, text)
+    column_number = col(error.loc, text)
+    lines = text.splitlines()
+    quote_symbol = "> "
+    error_indent = " " * (len(quote_symbol) + column_number)
+
+    error_message: list[str] = []
+    for line in lines[line_number - 5 : line_number]:
+        error_message.append(f"{quote_symbol}{line}")
+    error_message.append(f"{error_indent}^")
+    error_message.append(f"{error_indent}{error}")
+    for line in lines[line_number : line_number + 1]:
+        error_message.append(f"{quote_symbol}{line}")
+
+    return "\n".join(error_message)
+
 
 # -- Grammar ------------------------------------------------------------------
 
